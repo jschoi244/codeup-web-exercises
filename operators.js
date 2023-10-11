d6a22fe6f272c0bc97773990bca40143dd27b3f0
Justin Choi <justinchoi244@gmail.com>
2023-10-11 15:35:21 -0500

    Added operators.js

---
+++ operators.js
@@ -0,0 +1,36 @@
+// console.log('1' + 2);
+// console.log("" ** 4);
+// console.log(typeof '1' + 2);
+// console.log(6 % 4);
+// console.log('3 + 4 is ' + (3 + 4));
+// console.log(0 < 0);
+// console.log('false' == false);
+// console.log(true || "42");
+// console.log(!true && !false);
+// console.log(6 % 5);
+// console.log(5 < 4 && 1 === 1);
+// console.log(typeof 'codeup' === 'string');
+// console.log('codeup' === 'codeup' && 'codeup' === 'Codeup');
+// console.log(4 >= 0 &&  1 !== '1');
+// console.log(6 % 3 === 0);
+// console.log(5 % 2 !== 0);
+const logStatements = {
+    statement1: '1' + 2,
+    statement2: "" ** 4,
+    statement3: typeof '1' + 2,
+    statement4: 6 % 4,
+    statement5: '3 + 4 is ' + (3 + 4),
+    statement6: 0 < 0,
+    statement7: 'false' == false,
+    statement8: true || "42",
+    statement9: !true && !false,
+    statement10: 6 % 5,
+    statement11: 5 < 4 && 1 === 1,
+    statement12: typeof 'codeup' === 'string',
+    statement13: 'codeup' === 'codeup' && 'codeup' === 'Codeup',
+    statement14: 4 >= 0 && 1 !== '1',
+    statement15: 6 % 3 === 0,
+    statement16: 5 % 2 !== 0
+};
+
+console.log(logStatements);
