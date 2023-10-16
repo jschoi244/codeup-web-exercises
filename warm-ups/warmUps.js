d27a48f1c288f3d4c93f18ba28f8379c6f6ea905
Justin Choi <justinchoi244@gmail.com>
2023-10-16 09:08:08 -0500

    10/16/23 warmup

--- warm-ups/warmUps.js
+++ warm-ups/warmUps.js
@@ -3,3 +3,9 @@
 let name = "Justin Choi";
 let year = "2010";
 console.log(`Hi my name is ${name} and I graduated high school in ${year}.`);
+
+// 10/16/2023 Warmup
+
+const capitalizeFirstLetter = (word) => word.charAt(0).toUpperCase() + word.slice(1)
+console.log(capitalizeFirstLetter("codeup"));
+
