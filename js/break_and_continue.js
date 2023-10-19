84021fa1202f4662b28de373bf53328d4f4f5709
Justin Choi <justinchoi244@gmail.com>
2023-10-19 15:10:20 -0500

    Fix break and continue conditional

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,7 +1,7 @@
 let userInput;
-while (isNaN(userInput) || userInput % 2 === 0) {
+while (isNaN(userInput) || userInput % 2 === 0 || userInput < 1 || userInput > 50) {
     userInput = parseInt(prompt("Enter an odd number:"));
-    console.log(userInput);
+     console.log(userInput);
 }
 
 for (let i = 1; i <= 50; i++) {
@@ -14,3 +14,4 @@ for (let i = 1; i <= 50; i++) {
     }
     console.log(i);
 }
+
