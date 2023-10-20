6bbd7c5b7d0eb59cfd1836b5a35bf2dbdfdcdaac
Justin Choi <justinchoi244@gmail.com>
2023-10-20 10:50:52 -0500

    Iterating arrays exercise

--- js/operators.js
+++ js/operators.js
@@ -14,23 +14,23 @@
 // console.log(4 >= 0 &&  1 !== '1');
 // console.log(6 % 3 === 0);
 // console.log(5 % 2 !== 0);
-const logStatements = {
-    statement1: '1' + 2,
-    statement2: "" ** 4,
-    statement3: typeof '1' + 2,
-    statement4: 6 % 4,
-    statement5: '3 + 4 is ' + (3 + 4),
-    statement6: 0 < 0,
-    statement7: 'false' == false,
-    statement8: true || "42",
-    statement9: !true && !false,
-    statement10: 6 % 5,
-    statement11: 5 < 4 && 1 === 1,
-    statement12: typeof 'codeup' === 'string',
-    statement13: 'codeup' === 'codeup' && 'codeup' === 'Codeup',
-    statement14: 4 >= 0 && 1 !== '1',
-    statement15: 6 % 3 === 0,
-    statement16: 5 % 2 !== 0
-};
+// const logStatements = {
+//     statement1: '1' + 2,
+//     statement2: "" ** 4,
+//     statement3: typeof '1' + 2,
+//     statement4: 6 % 4,
+//     statement5: '3 + 4 is ' + (3 + 4),
+//     statement6: 0 < 0,
+//     statement7: 'false' == false,
+//     statement8: true || "42",
+//     statement9: !true && !false,
+//     statement10: 6 % 5,
+//     statement11: 5 < 4 && 1 === 1,
+//     statement12: typeof 'codeup' === 'string',
+//     statement13: 'codeup' === 'codeup' && 'codeup' === 'Codeup',
+//     statement14: 4 >= 0 && 1 !== '1',
+//     statement15: 6 % 3 === 0,
+//     statement16: 5 % 2 !== 0
+// };
 
 console.log(logStatements);
